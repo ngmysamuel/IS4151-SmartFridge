b644c8e839cceb11ea2f69211ee5ad3d9050e88c
ngmysamuel <e0148675@u.nus.edu>
2020-04-26 11:58:12 +0800

    Added method implementations and changed yaml to query params

--- sensor_raspi/sensor_raspi_tables.sql
+++ sensor_raspi/sensor_raspi_tables.sql
@@ -11,3 +11,18 @@ CREATE TABLE door (
     timestamp DATETIME NOT NULL,
     tocloud INTEGER DEFAULT 0 NOT NULL
 );
+
+
+SELECT * FROM temperature;
+SELECT * FROM door;
+
+select * from door where door_open = 1;
+SELECT door_open, timestamp FROM door WHERE timestamp BETWEEN "2020-04-25 20:41:26" AND "2020-04-25 20:43:04" AND door_open = 1;
+INSERT INTO door (door_open, timestamp) VALUES (1, "2020-04-26 12:02:30");
+INSERT INTO door (door_open, timestamp) VALUES (0, "2020-04-26 12:02:45");
+
+DELETE FROM temperature;
+
+DROP TABLE temperature;
+
+sqlite3 sensor_data;
